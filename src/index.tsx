2394fd557d4957f32130f5bfd9e3a1060a4d9f94
avi1737 <avinashvarpeti1@gmail.com>
2022-10-07 11:07:55 +0530

    downgrade react version

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
+import { render } from "react-dom";
 
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/store";
@@ -7,17 +8,21 @@ import { Provider } from "react-redux";
 
 import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+
+const root = document.getElementById("root");
+
+render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  root
 );
 
 // If you want to start measuring performance in your app, pass a function
